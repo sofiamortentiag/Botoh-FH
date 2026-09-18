53b636d17ae94a7300dac4e71b92c77e4c1ac230
Nanoseb <Nanoseb33@gmail.com>
2026-09-18 11:17:28 -0300

    fix: update HAXBALL 25 MIN tire durability values

--- Botoh/src/features/tires&pits/tires.ts
+++ Botoh/src/features/tires&pits/tires.ts
@@ -94,9 +94,9 @@ export const TYRE_DURABILITY = (limit: number | null): TireDurability => {
     // };
     // FORMULA HAXBALL 25 MIN TIRES
     return {
-      [Tires.SOFT]: 1200, // ≈ 840 = 14
-      [Tires.MEDIUM]: 1900, // ≈ 980 = 16,3
-      [Tires.HARD]: 2600, // ≈ 1260 = 21
+      [Tires.SOFT]: 525, // ≈ 840 = 14
+      [Tires.MEDIUM]: 875, // ≈ 980 = 16,3
+      [Tires.HARD]: 1225, // ≈ 1260 = 21
       [Tires.WET]: 1200 * 0.7, // ≈ 840
       [Tires.INTER]: 1200 * 0.7, // ≈ 840
       [Tires.FLAT]: Infinity,
